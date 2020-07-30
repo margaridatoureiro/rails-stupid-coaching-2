ae6aed2c98a7238f8720f4d225f5014fef3080bf
Margarida Toureiro <margaridatoureiro@gmail.com>
2020-07-30 15:39:52 +0100

    done as a TA now

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'questions#home'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  # verb "url", to: "controller#action"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
